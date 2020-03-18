164b6f63f1a440efe76328d5796e3212402b978c
saum7800 <gandhisaumya8@gmail.com>
2020-03-18 15:19:09 +0530

    completed tokenizing

--- tokenizing.py
+++ tokenizing.py
@@ -1,5 +1,5 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-example_text = "this is just to check how's the weather today tokenizer"
+example_text = "this is just to check how's the weather today tokenizer. Splitting into multiple sentences here"
 print(word_tokenize(example_text))
-
+print(sent_tokenize(example_text))
